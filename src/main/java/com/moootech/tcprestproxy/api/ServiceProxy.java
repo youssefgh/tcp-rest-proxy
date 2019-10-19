f2ed55ef56a72a0efc539bb1c6ebad61aa607cc4
Youssef GHOUBACH <ghoubach.youssef@gmail.com>
2019-10-19 18:30:16 +0000

    server and port supported as args

--- src/main/java/com/moootech/tcprestproxy/api/ServiceProxy.java
+++ src/main/java/com/moootech/tcprestproxy/api/ServiceProxy.java
@@ -14,12 +14,14 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import service.StartupBean;
 
 /**
  *
@@ -28,16 +30,15 @@ import javax.ws.rs.core.Response;
 @Path("proxy")
 @RequestScoped
 public class ServiceProxy {
+    
+    @Inject
+    private StartupBean startupBean;
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response submit(Call call) throws IOException {
-        //TODO read from param
-        if (call.getPort() < 50001 || call.getPort() > 50200) {
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        }
-        var socket = new Socket("172.17.0.1", call.getPort());
+        var socket = new Socket(startupBean.getServer(), startupBean.getPort());
         socket.setSoTimeout(10000);
         var printWriter = new PrintWriter(socket.getOutputStream());
         call.getProcedureList().forEach((procedure) -> {
---
+++ src/main/java/service/StartupBean.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
+
+/**
+ *
+ * @author youssef
+ */
+@ApplicationScoped
+public class StartupBean {
+
+    private String server;
+    private int port;
+
+    public void init(@Observes @Initialized(ApplicationScoped.class) Object init) {
+        var serverArg = System.getProperty("server");
+        var portArg = System.getProperty("port");
+        if (serverArg != null && portArg != null) {
+            this.server = serverArg;
+            this.port = Integer.parseInt(serverArg);
+        } else {
+            this.server = "localhost";
+            this.port = 50001;
+        }
+        Logger.getLogger(StartupBean.class.getName()).log(Level.INFO, "Using {0}:{1} as destination", new Object[]{this.server, this.port});
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+}
